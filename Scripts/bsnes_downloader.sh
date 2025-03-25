2e530c2f14c0631f5fedb7fbe1f9fd6111c3784f
Megalon <megalonsba@gmail.com>
2025-03-25 03:54:45 -0400

    a bunch of changes that rely on pegasus

---
+++ Scripts/bsnes_downloader.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+# Ensure the destination directory exists
+DEST_DIR="/Applications/Emulators"
+mkdir -p "$DEST_DIR"
+
+url=$(curl -s https://api.github.com/repos/bsnes-emu/bsnes/releases \
+    | jq -r '[.[] | select(.assets)] | map(.assets[]) | map(select(.name | contains("macos"))) | first | .browser_download_url')
+
+
+# Check if URL was fetched correctly
+if [[ -z "$url" ]]; then
+    echo "Error: Could not fetch the download URL. Please check your internet connection or the repository."
+    exit 1
+fi
+
+# Download the latest release
+output_file="$DEST_DIR/bsnes-macos-latest.zip"
+echo
+echo "Downloading latest bsnes build..."
+curl -L "$url" -o "$output_file" > /dev/null 2>&1
+
+# Check if the download was successful
+if [[ ! -f "$output_file" ]]; then
+    echo "Error: Download failed. Please check your internet connection."
+    exit 1
+fi
+
+# Extract the ZIP file
+cd "$DEST_DIR" || exit 1
+unzip bsnes-macos-latest.zip > /dev/null 2>&1
+
+# Move binary
+mv bsnes-*/bsnes.app .
+rm -rf bsnes-*
+
+# Remove the ZIP file
+rm -f bsnes-macos-latest.zip
+echo "Latest bsnes build downloaded."
+echo
+
+# Return to the original directory
+cd - >/dev/null || exit 1
